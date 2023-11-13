8dc620e738c293182cbf5b0fae561b1022035fae
Sahidev1 <theoddrogue@gmail.com>
2023-11-13 16:45:39 +0100

    Updated chatgpt.js module with latest openai JS libarary version

--- chatgpt.js
+++ chatgpt.js
@@ -1,47 +1,38 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { OpenAI } = require("openai");
+const { readConfig } = require("./filehandler");
 require('dotenv').config({
     path: require('path').resolve(__dirname, './.env')
 });  
-const { stdoutDataStreamFilter } = require("./util");
-const { readConfig } = require("./filehandler");
 
 const jsonConfig = readConfig();
 
-const config = new Configuration({
+const config = {
     apiKey: process.env.OPENAI_API_KEY || jsonConfig.OPENAI_API_KEY,
-});
+};
 
-const openai = new OpenAIApi(config);
+const openai = new OpenAI(config);
 
-//valid roles for prompt: user and system
 const prompt = async (messages) => {
+    console.log(messages);
     if(!process.env.OPENAI_API_KEY && !jsonConfig.OPENAI_API_KEY){
         throw new Error('No OPEN_API_KEY detected, run bashgpt with -k parameter with your openai key to set keys or change keys')
     }
     try {
-        const completion = await openai.createChatCompletion({
+        const stream = await openai.chat.completions.create({
             model: jsonConfig.GPT_MODEL||"gpt-3.5-turbo",
             messages: messages,
             stream:true,
-        }, { responseType: 'stream'});
-        console.log(`MESSAGE:\n`);
-        let accumData = "";
-
-        return new Promise((resolve, reject) => {
-            completion.data.on('data', (data) => {
-                let content = stdoutDataStreamFilter(data);
-                accumData = accumData.concat(content);
-            });
-            completion.data.on('end', () => {
-                resolve(accumData);
-            });
-            completion.data.on('error', (err) => {
-                reject(err);
-            });
         });
-
+        let accumdata = "";
+        let chunkdata = "";
+        for await (const chunk of stream){
+            chunkdata = chunk.choices[0]?.delta?.content || '';
+            process.stdout.write(chunkdata);
+            accumdata = accumdata.concat(chunkdata);
+        }
+        return accumdata;
     } catch (error) {
-        console.error('If your openai key doesnt work and you want to change it use bashgpt with -k option along with new openai key to change it', error.message);
+        console.error('If your openai key doesnt work and you want to change it use bashgpt with -k option along with new openai key to change it', error);
     }
 }
 
