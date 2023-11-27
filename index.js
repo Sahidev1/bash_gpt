c4bccfc34c762d92ae5900299f4c645fe74b0713
Sahidev1 <theoddrogue@gmail.com>
2023-11-27 21:29:51 +0100

    restructure

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const readline = require("readline");
 const prompt = require ("./chatgpt");
 const {parseArgs} = require ("./opthandler");
 const {getMsgArrFromTmp, writeMsgArrToTmp, writeConfig, readConfig, clearSessionHistory} = require("./filehandler");
-const { stdin } = require("process");
+
+
 
 const mainf = async () => 
 {
@@ -15,18 +16,14 @@ const mainf = async () =>
             config.OPENAI_API_KEY = args.api_key;
             writeConfig(config);
             console.log("API keys updated\nIf prompting causes errors try opening a new terminal window\n");
-            process.exit(0);
         }
-
-        if (args.model){
+        else if (args.model){
             const config = readConfig();
             config.GPT_MODEL = args.model;
             writeConfig(config);
             console.log("GPT Model updated!");
-            process.exit(0);
         }
-
-        if (args.clear_history){
+        else if (args.clear_history){
             const scanStdin = async (question) => {
                 const rl = readline.createInterface({
                     input: process.stdin,
@@ -39,29 +36,26 @@ const mainf = async () =>
                     });
                 });
             };
-            try {
-                const answer = await scanStdin("Are you sure you want to clear session history? (y/n): ");
-                if (answer === "y"){
-                    clearSessionHistory();
-                    console.log("Session history cleared!");
-                }
-                else {
-                    console.log("Session history not cleared!");
-                }
-                process.exit(0);
-            } catch (error) {
-                console.error('error', error);
-            }      
+   
+            const answer = await scanStdin("Are you sure you want to clear session history? (y/n): ");
+            if (answer === "y"){
+                clearSessionHistory();
+                console.log("Session history cleared!");
+            }
+            else {
+                console.log("Session history not cleared!");
+            }
+        } else {
+            const msgArr = getMsgArrFromTmp();
+            const updatedMsgArr = [...msgArr, {role: args.role || "user", content: args.prompt}];
+            const responsemsg = await prompt(updatedMsgArr);
+            writeMsgArrToTmp([...updatedMsgArr, {role: "assistant", content: responsemsg}]);
         }
-
-        const msgArr = getMsgArrFromTmp();
-        const updatedMsgArr = [...msgArr, {role: args.role || "user", content: args.prompt}];
-        const responsemsg = await prompt(updatedMsgArr);
-        writeMsgArrToTmp([...updatedMsgArr, {role: "assistant", content: responsemsg}]);
     } catch (error) {
         console.error('error', error);
     }
 }
+
 
 mainf();
 
